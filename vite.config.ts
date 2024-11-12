cb149dd8d91983246ef885662ab8da7bba81882a
JSSatorres <juansataz.dev@gmail.com>
2024-11-12 17:50:56 +0100

    feat: config folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import * as path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({ mode }) => {
+  const envDir = path.resolve(__dirname, 'env')
+  const env = loadEnv(mode, envDir) as Record<string, string>
+
+  return {
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    define: {
+      'process.env': env,
+    },
   }
 })
