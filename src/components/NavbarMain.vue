3c69c250ddc587068041b12ccfee9cc8a025d369
JSSatorres <juansataz.dev@gmail.com>
2024-11-15 18:53:43 +0100

    feat: implement NavbarMain styles and functionality for improved user experience and navigation consistency

--- src/components/NavbarMain.vue
+++ src/components/NavbarMain.vue
@@ -5,83 +5,54 @@ import {
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarSeparator,
   MenubarShortcut,
   MenubarTrigger,
 } from '@/components/ui/menubar'
 
 const navigationItems = [
-  { name: 'Home', to: '/' },
-  { name: 'Market', to: '/market' },
+  {
+    name: 'Home',
+    to: '/',
+  },
+  {
+    name: 'Market',
+    children: ['Hoasdasdme', 'Masaddasdrket', 'Tdasdaseam', 'Modasdasdaney'],
+  },
   { name: 'Team', to: '/team' },
-  { name: 'Money', to: '/money' },
+  {
+    name: 'Money',
+    children: [
+      'Homsadsade',
+      'Marksadasdet',
+      'Teamsadsadsa',
+      'Masdsadasdasoney',
+    ],
+  },
 ]
 </script>
 
 <template>
-  <Menubar>
-    <!-- <MenubarMenu>
-      <MenubarTrigger>
-        <RouterLink class="text-2xl" to="/">Home</RouterLink>
-      </MenubarTrigger>
-    </MenubarMenu> -->
-
-    <MenubarMenu>
-      {{ navigationItems }}
-      <MenubarTrigger>
-        <RouterLink class="text-2xl" to="/market">Market</RouterLink>
-      </MenubarTrigger>
-      <MenubarContent>
-        <MenubarItem>
-          <RouterLink to="/market/view">View Market</RouterLink>
-          <MenubarShortcut>⌘M</MenubarShortcut>
-        </MenubarItem>
-        <MenubarItem>
-          <RouterLink to="/market/analysis">Market Analysis</RouterLink>
-        </MenubarItem>
-        <MenubarSeparator />
-        <MenubarItem>
-          <RouterLink to="/market/trends">Trends</RouterLink>
-        </MenubarItem>
-      </MenubarContent>
-    </MenubarMenu>
-
-    <MenubarMenu>
-      <MenubarTrigger>
-        <RouterLink to="/team">Team</RouterLink>
-      </MenubarTrigger>
-      <MenubarContent>
-        <MenubarItem>
-          <RouterLink to="/team/view">View Team</RouterLink>
-          <MenubarShortcut>⌘T</MenubarShortcut>
-        </MenubarItem>
-        <MenubarItem>
-          <RouterLink to="/team/performance">Team Performance</RouterLink>
-        </MenubarItem>
-        <MenubarSeparator />
-        <MenubarItem>
-          <RouterLink to="/team/manage">Manage Members</RouterLink>
-        </MenubarItem>
-      </MenubarContent>
-    </MenubarMenu>
-
-    <MenubarMenu>
-      <MenubarTrigger>
-        <RouterLink to="/money">Money</RouterLink>
-      </MenubarTrigger>
-      <MenubarContent>
-        <MenubarItem>
-          <RouterLink to="/money/view">View Finances</RouterLink>
-          <MenubarShortcut>⌘F</MenubarShortcut>
-        </MenubarItem>
-        <MenubarItem>
-          <RouterLink to="/money/transactions">Transactions</RouterLink>
-        </MenubarItem>
-        <MenubarSeparator />
-        <MenubarItem>
-          <RouterLink to="/money/reports">Reports</RouterLink>
-        </MenubarItem>
-      </MenubarContent>
+  <Menubar class="w-3/4 flex items-center justify-center">
+    <MenubarMenu v-for="(item, index) in navigationItems" :key="index">
+      <template v-if="item.children">
+        <MenubarTrigger class="text-2xl">{{ item.name }}</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem
+            v-for="(child, childIndex) in item.children"
+            :key="childIndex"
+          >
+            <RouterLink :to="'/' + child">{{ child }}</RouterLink>
+            <MenubarShortcut>⌘T</MenubarShortcut>
+          </MenubarItem>
+        </MenubarContent>
+      </template>
+      <template v-else>
+        <MenubarTrigger>
+          <RouterLink :to="item.to" class="text-2xl">{{
+            item.name
+          }}</RouterLink>
+        </MenubarTrigger>
+      </template>
     </MenubarMenu>
   </Menubar>
 </template>
