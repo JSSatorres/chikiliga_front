1fb039d66a881cd01b86cd2518f8a2c635f67a26
JSSatorres <juansataz.dev@gmail.com>
2024-12-15 20:21:26 +0100

    chore: remove commented-out code from HomePage.vue for cleaner structure

--- src/features/home/HomePage.vue
+++ src/features/home/HomePage.vue
@@ -1,29 +1,4 @@
-<script setup lang="ts">
-// import { ref, onMounted } from 'vue'
-
-// Definir la URL de la API a través de una variable de entorno
-// const apiUrl = import.meta.env.VITE_API_URL
-
-// Función para obtener datos desde la API (opcional, comentada en este caso)
-// const fetchTeam = async () => {
-//   try {
-//     const response = await fetch(`${apiUrl}/team`)
-//     const data = await response.json()
-//     return data
-//   } catch (error) {
-//     console.error('Error fetching data:', error)
-//     return [] // Devuelve un array vacío si ocurre un error
-//   }
-// }
-
-// Estado reactivo para almacenar los datos (comentar si no es necesario)
-// const items = ref<any[]>([])
-
-// Llamar a la API cuando el componente se monte (opcional, comentado aquí)
-// onMounted(async () => {
-//   items.value = await fetchTeam()
-// })
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div
